1b4ffddbfc7a72c35cbf635fef543875fda466df
William Brege <william.brege@gmail.com>
2017-04-16 23:51:53 -0700

    Barebones code

---
+++ siete_y_medio.cpp
@@ -0,0 +1,26 @@
+/*  @file siete_y_medio.cpp
+    @author William Brege
+    ID: 704-482-379
+    @date 04/16/2017
+ 
+    This program plays siete y medio with the user until either the player or the computer has run out of money
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "cards.h"
+
+int main() {
+    bool playing = true;
+    
+    
+    while(playing == true){
+        
+    }
+    
+    return 0;
+}

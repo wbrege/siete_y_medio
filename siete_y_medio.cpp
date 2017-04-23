00fc4af1b78c76b7d05d401a4e65e08ff8f69b7b
William Brege <william.brege@gmail.com>
2017-04-23 15:11:45 -0700

    Finished dealing to user implementation

--- siete_y_medio.cpp
+++ siete_y_medio.cpp
@@ -15,7 +15,7 @@
 #include "cards.h"
 
 int main() {
-    bool playing = true, dealing = true;
+    bool playing = true;
     Player user(100), dealer(900);
     int bet;
     double playerTotal;
@@ -24,6 +24,7 @@ int main() {
     srand(time(nullptr));
     
     while(playing == true){
+        bool dealing = true;
         std::cout << "You have $" << user.checkMoney() << ". Enter bet: ";
         std::cin >> bet;
         //Check if valid bet size
@@ -41,12 +42,29 @@ int main() {
             playerTotal = playerHand.getTotal()/2.;
             std::cout << "Your total is " << playerTotal << ". Do you want another card (y/n)? ";
             std::cin >> response;
+            //Check response
             if(response == 'n'){
                 dealing = false;
                 break;
             }
             Card newCard;
             playerHand.push_back(newCard);
+            
+            //Check if over 7.5
+            if(playerHand.getTotal() >= 15){
+                std::cout << "Your cards:" << std::endl;
+                playerHand.printCards();
+                playerTotal = playerHand.getTotal()/2.;
+                std::cout << "Your total is " << playerTotal << std::endl;
+                std::cout << "Too bad. You lose " << bet;
+                user.changeMoney(-bet);
+                if(user.isBankrupt()){
+                    dealing = false;
+                    playing = false;
+                    std::cout << "You have $0. GAME OVER!" << std::endl << "Come back when you have more money!" << std::endl << std::endl << "Bye!";
+                    return 0;
+                }
+            }
         }
         
         
